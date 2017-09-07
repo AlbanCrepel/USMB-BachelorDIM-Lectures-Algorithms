3c0f6a7492f97607626a0e2bac35852348b364c0
AlbanCrepel <alban.crepel@gmail.com>
2017-09-07 12:04:59 +0200

    Max value function added

--- assignments/Session1/S1_algotools.py
+++ assignments/Session1/S1_algotools.py
@@ -4,6 +4,10 @@
 """
 
 def average_above_zero(list):
+    """
+    Basic function able to return the average of a list's values
+    @param list : the list to be scanned
+    """
     sum = 0
     n = 0
     for i in list:
@@ -21,3 +25,28 @@ result = average_above_zero(my_list)
 message = "The average of positive elements of {list_values} is : {res}".format(list_values=my_list,res=result)
 print(message)
 """
+
+def max_value(list):
+    """
+    Basic function able to return the max value of a list
+    @param list : the list to be scanned
+    @throws a exception (ValueError) on an empty list
+    """
+    if len(list) == 0:
+        raise ValueError("The list is empty")
+        
+    max = list[0]
+    index_of_max = 0
+    for index,item in enumerate(list):
+        if item > max:
+            max = item
+            index_of_max = index
+    return max
+            
+#testing max_value function
+my_list = [1,2,3,4,-7]
+result = max_value(my_list)
+message = "The max value of {list_values} is : {res}".format(list_values=my_list,res=result)
+print(message)
+            
+    
