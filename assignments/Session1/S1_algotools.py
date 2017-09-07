4d91f7f9a5c9cdb30457e929a7a36c5ed61fcfc7
AlbanCrepel <alban.crepel@gmail.com>
2017-09-07 13:36:16 +0200

    Answer for question 2

--- assignments/Session1/S1_algotools.py
+++ assignments/Session1/S1_algotools.py
@@ -3,14 +3,14 @@
 @brief : a set of generic functions for data management
 """
 
-def average_above_zero(list):
+def average_above_zero(table):
     """
     Basic function able to return the average of a list's values
     @param list : the list to be scanned
     """
     sum = 0
     n = 0
-    for i in list:
+    for i in table:
         #select only positive values
         if i > 0:
             sum += i
@@ -26,27 +26,29 @@ message = "The average of positive elements of {list_values} is : {res}".format(
 print(message)
 """
 
-def max_value(list):
+def max_value(table):
     """
     Basic function able to return the max value of a list
     @param list : the list to be scanned
     @throws a exception (ValueError) on an empty list
     """
-    if len(list) == 0:
+    if len(table) == 0:
         raise ValueError("The list is empty")
         
-    max = list[0]
-    index_of_max = 0
-    for index,item in enumerate(list):
-        if item > max:
-            max = item
-            index_of_max = index
-    return max
-            
+    max_value = table[0]
+    index_of_max_value = 0
+    for index,item in enumerate(table):
+        if item > max_value:
+            max_value = item
+            index_of_max_value = index
+    return max_value,index_of_max_value
+           
+  
+"""        
 #testing max_value function
 my_list = [1,2,3,4,-7]
 result = max_value(my_list)
-message = "The max value of {list_values} is : {res}".format(list_values=my_list,res=result)
+message = "The max value of {list_values} is : {value} at index {index}".format(list_values=my_list,value=result[0],index=result[1])
 print(message)
-            
-    
+"""
+
