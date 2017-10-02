a53c0e77ef8ed2812d702637f6d6740bc025da90
AlbanCrepel <alban.crepel@gmail.com>
2017-10-02 11:22:49 +0200

    test of max_value added

--- assignments/Session1/test_S2.py
+++ assignments/Session1/test_S2.py
@@ -31,3 +31,35 @@ def test_average_above_zero_with_positive_values():
 	assert algo.average_above_zero(list) == 2.5
 
 
+def test_max_value_with_empty_list():
+	"""
+	Function that tests the function max_value 
+	with an empty list
+	"""
+	list = []
+	with pytest.raises(ValueError):
+		res = algo.max_value(list)
+
+def test_max_value_with_positive_values():
+	"""
+	Function that tests the function max_value 
+	with a list of positive values
+	"""
+	list = [1,2,3,4,7]
+	assert algo.max_value(list) == (7,4)
+
+def test_max_value_with_negative_values():
+	"""
+	Function that tests the function max_value 
+	with a list of negative values
+	"""
+	list = [-1,-2,-3,-4,-7]
+	assert algo.max_value(list) == (-1,0)
+
+def test_max_value_with_negative_and_positive_values():
+	"""
+	Function that tests the function max_value 
+	with a list of both negative and positive values
+	"""
+	list = [1,-2,-3,-4,-7]
+	assert algo.max_value(list) == (1,0)
